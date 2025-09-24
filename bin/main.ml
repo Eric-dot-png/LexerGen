6286ce5d9fe5198b9c3acc359ba19ec0037689dc
Eric Garcia <egarciaf@me.com>
2025-09-24 11:19:50 -0500

    added arg parsing

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,10 @@
-(*
-file  : main.ml
-brief : app entry point
-*)
+(*----------------------------------------------------------------------------*)
+(* file  : main.ml                                                            *)
+(* brief : application entry point and arg parser code                        *)
+(*----------------------------------------------------------------------------*)
 
 open MyLexing
 open MyParsing
-
 
 let sample_file = 
 "
@@ -26,9 +25,36 @@ int main()
 
 "
 
+(*----------------------------------------------------------------------------*)
+(* Arg parser references and functions                                        *)
+(*----------------------------------------------------------------------------*)
+let parse_args : string * string * bool =
+  let input_filename = ref "none" in
+  let output_filename = ref "default" in
+  let debug_msgs = ref false in
+  let arg_specs = [
+    ("-i", Arg.Set_string input_filename, "Set input filename (required)");
+    ("-o", Arg.Set_string output_filename,"Set output filename (default : <input filename>.cpp/ml)");
+    ("-d", Arg.Set debug_msgs,"Set the program to print debug messages");
+  ] in
+  let usage_msg = "Usage:\ndune exec -- ./bin/main.exe [-i <filename>] [-o <filename>] [-d] <anonymous_args>" in
+  let _ = Arg.parse arg_specs
+    (fun anon_arg ->
+       let _ = Printf.printf "Unexpected argument: \"%s\"\n" anon_arg in
+       let _ = Arg.usage arg_specs usage_msg in
+       exit 1) usage_msg 
+  in 
+  !input_filename, !output_filename, !debug_msgs
 
-
+(*----------------------------------------------------------------------------*)
+(* Application entry point                                                    *)
+(*----------------------------------------------------------------------------*)
 let () = 
+  let input_filename, output_filename, debug_msgs = parse_args in 
+  let _ = Printf.printf "Input Filename: %s\n" input_filename in
+  let _ = Printf.printf "Output Filename: %s\n" output_filename in
+  let _ = Printf.printf "Debug: %b\n" debug_msgs in
+  
   let str = sample_file in
   let toks = MyLexing.lexAll str in
   let lex_file = MyParsing.parse toks in 
--- makefile
+++ makefile
@@ -1,14 +1,20 @@
+.PHONY: run 
+
+IFILE ?= in.txt
+OFILE ?= out.txt
+DEBUG ?= true
+
 run:
-	dune exec -- ./bin/main.exe
+	@dune exec -- ./bin/main.exe -i $(IFILE) -o $(OFILE) $(if $(filter true,$(DEBUG)),-d)
 
 log:
-	dune exec -- ./bin/main.exe > out.log
+	@dune exec -- ./bin/main.exe > out.log
 
 auto:
-	dune build --watch
+	@dune build --watch
 
 clean:
-	dune clean
+	@dune clean
 
 build:
-	dune build
+	@dune build
